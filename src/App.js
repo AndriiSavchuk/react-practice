037144ac11041a0add4faf752166d292236a5e4b
Andrii Savchuk <andriksr@gmail.com>
2017-05-23 22:14:11 +0300

    REact practice initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+/*const App = () => {
+    return (
+        <div>
+            <h1 className="title">Hello module from App</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, animi.</p>
+        </div>
+    );
+}
+
+export default App;*/
+
+export const App = () => {
+    return (
+        <div>
+            <h1 className="title">Hello module from App</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, animi.</p>
+        </div>
+    );
+}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+const css = require('./style.sass');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
+// import App from './App';
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
