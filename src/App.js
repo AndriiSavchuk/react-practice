e68bc43173dc0524655df4fe6b8f0e510f2df9e9
Andrii Savchuk <andriksr@gmail.com>
2017-06-02 20:12:39 +0200

    classes

--- src/App.js
+++ src/App.js
@@ -12,26 +12,50 @@ import PropTypes from 'prop-types';
 
 export default App;*/
 
-const HeadLine = () => {
-  return <h1 className="title">Hello from React App</h1>
-}
+// const HeadLine = () => {
+//   return <h1 className="title">Hello from React App</h1>
+// }
 
-const Greeting = (props) => {
-  const {name, number} = props;
-  return <p> {name} + {number} - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, animi.</p>
-}
+// const Greeting = (props) => {
+//   const {name, number} = props;
+//   return <p> {name} + {number} - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, animi.</p>
+// }
 
-
-export const App = () => {
+/*export const App = () => {
     return (
         <div>
             <HeadLine />
             <Greeting name="Property" number={25} />
         </div>
     );
+}*/
+
+// Greeting.propTypes = {
+//   name: PropTypes.string,
+// number: PropTypes.number, //number: React.PropTypes.number.isRequired requir to inndicate such property
+
+
+export class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      buyItems: ['milk', 'bread', 'fruits']
+    }
+  }
+
+  render() {
+    const {buyItems} = this.state;
+    return (
+        <div>
+          <h1>Shopping list</h1>
+          {
+            buyItems.map(item => {
+              return <p key={item}>{item}</p>
+            })
+          }
+        </div>
+    )
+  }
 }
 
-Greeting.propTypes = {
-  name: PropTypes.string,
-  number: PropTypes.number, //number: React.PropTypes.number.isRequired requir to inndicate such property
-}
