ef2fbd66ae6fc8448dc65b88134e5143b0b4a974
Andrii Savchuk <andriksr@gmail.com>
2017-06-12 19:30:12 +0200

    introduction to state

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,19 +1,31 @@
 /* function-based Component */
 
-import React from 'react';
+// import React from 'react';
 
-const SearchBar = () => {
-  return <input />;
-};
+// const SearchBar = () => {
+//   return <input />;
+// };
 
 /* class-based Component */
 
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
 
-// class SearchBar extends Component {
-//   render() {
-//     return <input />;
-//   }
-// };
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+        value={this.state.term}
+        onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ const App = () => {
   );
 }
 
-
 ReactDOM.render(
     <App />,
     document.getElementById('root')
